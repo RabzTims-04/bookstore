bc62714d8e2ccb3b99b2648b28ef546669387275
Rabz92 <rabbiailyas92@gmail.com>
2021-06-01 16:59:00 +0200

    small changes

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,22 +1,27 @@
 import { MDBIcon } from "mdbreact";
 
 const MyFooter = ()=>(
-    <div className="container footer">
-        <div className="row flex-column text-center text-muted align-items-center mt-5">            
-               <h1>{"Contact Us"}</h1>          
+    <div className="container footer ">
+        <div className="row text-light"> 
+        <div className = "col-6 pt-3">
+        <h1>{"Contact Us"}</h1>          
            
-                 <p className="mt-2">{"We are currently giving discounts on latest books. You can enjoy reading and can also donate books"}</p>         
-          
-                 <p className="mt-2">{"+44 (0) 2920 090 505"}</p>
-                 <p className="mt-2">{"rabzbookstore@gmail.com"}</p>
-         
-            <div className="mt-3 mb-5">
+           <p className="mt-2">{"We are currently giving discounts on latest books. You can enjoy reading and can also donate books"}</p>         
+    
+           <p className="mt-2">{"+44 (0) 2920 090 505"}</p>
+           <p className="mt-2">{"rabzbookstore@gmail.com"}</p>
+            </div>  
+
+            <div className="col-6 ml-auto mt-3 pb-5 text-right pt-5">            
                  <MDBIcon fab icon="facebook" size="2x" className="mr-2 indigo-text" />
                  <MDBIcon fab icon="instagram" size="2x" className="mr-2 orange-text" />
                  <MDBIcon fab icon="youtube" size="2x" className="mr-2 red-text"/>
                  <MDBIcon fab icon="snapchat" size="2x"className="mr-2 amber-text" />
-                 <MDBIcon fab icon="discord" size="2x" className="mr-2 cyan-text"/>
-            </div>
+                 <MDBIcon fab icon="discord" size="2x" className="mr-2 cyan-text"/>            
+            </div>         
+               
+         
+            
         </div>
     </div>
 )
